95f988f148b4369e9d39a9ae5de5166566848681
Esther Rayssa Domingos <106018315+estherrdomingos@users.noreply.github.com>
2023-06-13 11:59:05 -0300

    criação de funções: create, read e update

--- main.js
+++ main.js
@@ -1,20 +1,33 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
+    const closeModal = () => {
+        clearFields()
+        document.getElementById('modal').classList.remove('active')
+    }
 
     const tempClient = {
-        nome: "Fernando",
-        email: "fernandogmail@gmail.com",
+        nome: "nikolas",
+        email: "nikinho@gmail.com",
         celular: "123456789",
-        cidade: "rio de janeiro"
+        cidade: "sao roque"
     }
-// CRUD
+
+    const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
+// CRUD -----------------------------------------------------------------------------------------
+// CRUD CREATE------------------------------------------------------------------------------------
+const updateClient = (index, client) => {
+const dbClient = readClient()
+dbClient[index] = client
+}
+// CRUD READ----------------------------------------------------------------------------------------------
+const readClient = () => getLocalStorage()
+// CRUD CREATE ----------------------------------------------------------------------------------------------
 const createClient = (client) => {
-    const db_client = []
-    db_client.push(client)
-    localStorage.setItem('db_client',JSON.stringify(db_client))
+    const dbClient = getLocalStorage()
+    dbClient.push(client)
+    setLocalStorage(dbClient)
 }
 
     // EVENTOS 
